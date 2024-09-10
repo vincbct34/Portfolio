a3869f21bad938fdf8038d7568c5859ce433db79
Vincent Bichat <vbichat@epsa.com>
2024-09-10 17:40:07 +0200

    [UPD] Adjusting the dark mode toggle button and title + logo of the website

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import useLocalStorage from "use-local-storage";
+import { useTranslation } from 'react-i18next'
+
+import { ToggleDarkMode } from "./ToggleDarkMode";
+import { Navbar } from "./Navbar";
+import './Header.css';
+
+export const Header = () => {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
+  const { t, i18n } = useTranslation("global")
+
+  return (
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <div className="Language">
+        {t("languageSettings.chooseLanguage")}
+        <button onClick={() => i18n.changeLanguage("en")} style={{ padding: '20px' }}>
+          <img src="/us-flag.png" alt="English" />
+        </button>
+        <button onClick={() => i18n.changeLanguage("fr")}>
+          <img src="/france-flag.png" alt="French" />
+        </button>
+      </div>
+      <ToggleDarkMode isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+      <Navbar />
+    </div>
+  );
+};
