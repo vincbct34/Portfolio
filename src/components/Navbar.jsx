8afa2219cb3bb494fb912c6ecdedfabd9648fac6
vincbct34 <vincent.bichat@epitech.eu>
2024-10-03 11:57:56 +0200

    [FIX] Fixing navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,7 +97,7 @@ export const Navbar = () => {
 
   return (
     <div>
-      <nav className="fixed flex flex-col items-center text-center left-1/2 -translate-x-1/2 top-28 z-10 md:gap-10">
+      <nav className="fixed flex flex-col items-center text-center left-1/2 -translate-x-1/2 top-28 z-10 md:gap-10 2xl:top-10">
         <div className="hidden flex-col justify-center items-center md:flex">
           {currentPageLogo}
         </div>
